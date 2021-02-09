c2df18a992e12dc6091ca03ecebdc6b6bdf4c735
David <dvaquero@bilbomatica.es>
2021-02-09 17:09:53 +0100

    consolidando cambios de navegación

--- app/src/main/java/com/example/wordroomandroidkotlin/RecycleViewActivity.kt
+++ app/src/main/java/com/example/wordroomandroidkotlin/RecycleViewActivity.kt
@@ -3,6 +3,9 @@ package com.example.wordroomandroidkotlin
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 
 class RecycleViewActivity : AppCompatActivity() {
@@ -18,9 +21,17 @@ class RecycleViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_recycle_view)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment),appBarConfiguration)
+
 
     }
     fun pasaDato(id: Int){
         wordViewModel.selectedId = id
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp() || super.onSupportNavigateUp()
+        //return NavigationUI.navigateUp(drawer, navHost.navController) || super.onSupportNavigateUp()
     }
 }
